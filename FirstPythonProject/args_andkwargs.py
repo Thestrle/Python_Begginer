8e8c7c2f3ca540186e53806a48a0fb38de29a3b9
Thestrle <kushvahaalok@gmail.com>
2020-05-03 18:35:29 +0530

    commit

--- FirstPythonProject/args_andkwargs.py
+++ FirstPythonProject/args_andkwargs.py
@@ -3,3 +3,4 @@
 # While passing *args and **kwargs both in a function the order of recieving and sending params should be same
 #e.g. myfunc(10,20,30,40,fruit='apple',veg='tomato') def myfunc(*args,**kwargs) correct
 #e.g. myfunc(10,20,30,40,fruit='apple',veg='tomato',50) def myfunc(*args,**kwargs) wrong
+map 
