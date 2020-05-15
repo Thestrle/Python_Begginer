403ee842e4e5546758876df542e8aa3089157806
Thestrle <kushvahaalok@gmail.com>
2020-05-15 15:48:28 +0530

    BlackJackGame file with separate files

--- FirstPythonProject/correct.py
+++ FirstPythonProject/correct.py
@@ -1,1 +1,23 @@
-space_check(board, position)
+def player_choice(board):
+    position = 0
+    try:
+        position = int(input('Choose a position : (1-9)'))
+        while True:
+        #position = int(input('Choose a position : (1-9)'))
+        #return position
+        if position in range(1,10) or not space_check(board, position):
+            return position
+        else:
+            print("Given space in not empty")
+            continue
+    except ValueError:
+        print("Worng Position Entered. Please enter a number in range 1-9.")
+        continue
+
+def player_choice(board):
+    position = 0
+    try:
+        while True:
+            position = int(input('Choose a position : (1-9)'))
+            if board[position] == " ":
+                board[position] = 
---
+++ OOP_Project/BlackJack_Game/Deck.py
@@ -0,0 +1,29 @@
+import Cards
+import random
+
+class Deck():
+
+    suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+    ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+    values = {'Two':2, 'Three':3, 'Four':4, 'Five':4, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+
+    def __init__(self):
+        self.deck =[]
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Cards(suit,rank))
+
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n'+card.__str__()
+            return "The Deck has "+ deck_comp
+
+# In place shuffle. Will not return any thing but shuffle the existing deck
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+# To get a single card for hit
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
